3884354edcf2960b31e85f61baecf35366f98b33
valentin <val.holgado@gmail.com>
2016-05-24 00:26:55 -0300

    Custom relative layout for main list

---
+++ app/src/main/kotlin/ar/valhol/mlapp/SquaredRelativeLayout.kt
@@ -0,0 +1,21 @@
+package ar.valhol.mlapp
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+class SquaredRelativeLayout: RelativeLayout {
+
+    constructor(ctx: Context) : super(ctx) {}
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+
+}
